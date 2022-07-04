e11f75335990882d282eec0a9aa407f02d925c7f
Iuliia <iuliiaiavorskaia@gmail.com>
2022-07-04 15:07:15 +0300

    save LocalStorage

--- src/App.js
+++ src/App.js
@@ -38,7 +38,20 @@ class App extends Component {
       contacts: prevState.contacts.filter(contact => contact.id !== id),
     }));
   };
-
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+    if (parsedContacts) {
+      this.setState({
+        contacts: parsedContacts,
+      });
+    }
+  }
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
   render() {
     const normalize = this.state.filter.toLowerCase();
     const filterContacts = this.state.contacts.filter(contact =>
